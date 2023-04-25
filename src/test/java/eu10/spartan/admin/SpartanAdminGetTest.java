0427b0b9c2ce976b73cd00dd5c962044f7809bfe
Ali Karaoglan <karaoglan.a.m@gmail.com>
2023-04-25 18:06:42 +0300

    add static import

--- src/test/java/eu10/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/eu10/spartan/admin/SpartanAdminGetTest.java
@@ -2,12 +2,15 @@ package eu10.spartan.admin;
 
 import io.restassured.http.ContentType;
 import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.SerenityRest;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 import static io.restassured.RestAssured.baseURI;
+import static net.serenitybdd.rest.SerenityRest.given;
 
 
 @SerenityTest
@@ -25,6 +28,17 @@ public class SpartanAdminGetTest {
 				.and().auth().basic("admin","admin")
 		.when().get("/api/spartans")
 		.then().statusCode(200)
+				.and().contentType(ContentType.JSON);
+	}
+
+	@Test
+	public void getOneSpartan(){
+
+		given().accept(ContentType.JSON)
+				.and().auth().basic("admin","admin")
+				.and().pathParam("id",15)
+				.when().get("/api/spartans/{id}")
+				.then().statusCode(200)
 				.and().contentType(ContentType.JSON);
 	}
 
