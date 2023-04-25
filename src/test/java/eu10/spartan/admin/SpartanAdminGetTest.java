6cc228876129c976a083fedbee989d935f84e643
Ali Karaoglan <karaoglan.a.m@gmail.com>
2023-04-25 17:21:42 +0300

    first test

--- src/test/java/eu10/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/eu10/spartan/admin/SpartanAdminGetTest.java
@@ -1,6 +1,28 @@
 package eu10.spartan.admin;
 
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.baseURI;
+
 public class SpartanAdminGetTest {
 
+	@BeforeAll
+	public static void init(){
+		baseURI = "http://44.199.219.111:7000";
+	}
+
+	@Test
+	public void getAllSpartan(){
+
+		given().accept(ContentType.JSON)
+				.and().auth().basic("admin","admin")
+		.when().get("/api/spartans")
+		.then().statusCode(200)
+				.and().contentType(ContentType.JSON);
+	}
 
 }
